4e39e779d962dca9810651c166371d7e7ff25554
eva <eva.aedea@gmail.com>
2025-08-08 01:24:46 +0800

    working method for fetching device list

--- src/TpLinkCloudSession.ts
+++ src/TpLinkCloudSession.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 
 export class TpLinkCloudSession {
     public readonly token: string;
@@ -20,6 +21,35 @@ export class TpLinkCloudSession {
         this.netType = params.netType;
         this.locale = params.locale;
         this.userAgent = params.userAgent;
+    }
+
+    async getDevices(): Promise<Array<any>> {
+        const request = {
+            method: "POST",
+            url: "https://wap.tplinkcloud.com",
+            params: {
+                appName: this.appName,
+                termID: this.termID,
+                appVer: this.appVer,
+                ospf: this.ospf,
+                netType: this.netType,
+                locale: this.locale,
+                token: this.token
+            },
+            headers: {
+                "User-Agent": this.userAgent,
+                "Content-Type": "application/json"
+            },
+            data: { method: "getDeviceList" }
+        };
+
+        const response = await axios(request);
+
+        if( ! response.data || response.data.error_code !== 0 ){
+            throw new Error(`Failed to fetch devices: ${response.data.error_code}`);
+        }
+
+        return response.data.result.deviceList;
     }
 }
 
--- test/e2e/TpLinkCloud.test.ts
+++ test/e2e/TpLinkCloud.test.ts
@@ -8,24 +8,28 @@ describe(`TpLinkCloud`, function () {
     const sut = TpLinkCloud;
 
     describe(`login`, function(){
-        it(`should login with valid credentials`, async function () {
+        it(`should login and be able to display devices with valid credentials`, async function () {
             const cloud = new sut({
                 username: process.env.TPLINK_CLOUD_USERNAME || "user not set",
                 password: process.env.TPLINK_CLOUD_PASSWORD || "pass not set",
             });
 
-            const result = await cloud.login();
+            const session = await cloud.login();
 
-            Assert.isObject(result, "Login should return a session object");
-            Assert.isString(result.token, "Session token should be a string");
-            Assert.equal(cloud.termID, result.termID, "TermID should match the one used in login");
-            Assert.equal(cloud.appName, result.appName, "AppName should match the one used in login");
-            Assert.equal(cloud.appVer, result.appVer, "AppVer should match the one used in login");
-            Assert.equal(cloud.ospf, result.ospf, "OSPF should match the one used in login");
-            Assert.equal(cloud.netType, result.netType, "NetType should match the one used in login");
-            Assert.equal(cloud.locale, result.locale, "Locale should match the one used in login");
-            Assert.equal(cloud.userAgent, result.userAgent, "UserAgent should match the one used in login");
+            Assert.isObject(session, "Login should return a session object");
+            Assert.isString(session.token, "Session token should be a string");
+            Assert.equal(cloud.termID, session.termID, "TermID should match the one used in login");
+            Assert.equal(cloud.appName, session.appName, "AppName should match the one used in login");
+            Assert.equal(cloud.appVer, session.appVer, "AppVer should match the one used in login");
+            Assert.equal(cloud.ospf, session.ospf, "OSPF should match the one used in login");
+            Assert.equal(cloud.netType, session.netType, "NetType should match the one used in login");
+            Assert.equal(cloud.locale, session.locale, "Locale should match the one used in login");
+            Assert.equal(cloud.userAgent, session.userAgent, "UserAgent should match the one used in login");
 
+            // get devices
+            const devices = await session.getDevices();
+
+            Assert.isArray(devices);
         });
     });
 });
